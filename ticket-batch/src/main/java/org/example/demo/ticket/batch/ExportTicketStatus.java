b72646132f2ff01c6960c569b2639ea7670b220b
esspressoh <laurent.lecorps@gmail.com>
2018-05-09 20:31:07 +0200

    backup 3

--- ticket-batch/src/main/java/org/example/demo/ticket/batch/ExportTicketStatus.java
+++ ticket-batch/src/main/java/org/example/demo/ticket/batch/ExportTicketStatus.java
@@ -1,14 +1,31 @@
 package org.example.demo.ticket.batch;
 
+import org.example.demo.ticket.business.contract.manager.TicketManager;
+import org.example.demo.ticket.model.bean.ticket.Ticket;
+import org.example.demo.ticket.model.bean.ticket.TicketStatut;
+import org.example.demo.ticket.model.recherche.ticket.RechercheTicket;
+
+import java.util.List;
+
 /**
  * Created by esspressoh on 09.05.18.
  */
-public class ExportTicketStatus {
+public class ExportTicketStatus implements TicketManager{
 
     public ExportTicketStatus() {
     }
 
 
+//protected void Ecrire(TicketStatut TicketStatut.toString()
+
+    @Override
+    public List<Ticket> getListTicket(RechercheTicket pRechercheTicket) {
+        return null;
+    }
+
+    public List<Ticket> Ecrire(){
+
+    }
 
 
 }
---
+++ ticket-consumer/src/main/java/org/example/demo/ticket/consumer/impl/rowmapper/ticket/TicketStatutRM.java
@@ -0,0 +1,24 @@
+package org.example.demo.ticket.consumer.impl.rowmapper.ticket;
+
+import org.example.demo.ticket.model.bean.ticket.TicketStatut;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by esspressoh on 09.05.18.
+ */
+public class TicketStatutRM implements RowMapper<TicketStatut> {
+
+    public TicketStatut mapRow(ResultSet pRS, int pRowNum) throws SQLException {
+
+        TicketStatut vTicketStatut = new TicketStatut(pRS.getInt("id"));
+
+        vTicketStatut.setLibelle(pRS.getString("libelle"));
+
+        return vTicketStatut;
+
+    }
+
+}
